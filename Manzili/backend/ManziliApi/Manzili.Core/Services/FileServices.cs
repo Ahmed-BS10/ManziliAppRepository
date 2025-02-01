611490e37c0361a448d1d2506f865562eb13cba9
Ahmed-BS10 <157908930+Ahmed-BS10@users.noreply.github.com>
2025-02-01 13:54:31 +0300

    Add FileService with async image upload functionality
    The `FileService` class includes an `UploadImageAsync` method for handling
    asynchronous image uploads. The method checks for null or empty files, creates
    the target directory if it doesn't exist, generates a unique file name, and
    copies the file to the target directory. It returns the relative path on
    success or "FailedToUploadImage" on error, logging any exceptions.

---
+++ Manzili/backend/ManziliApi/Manzili.Core/Services/FileServices.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Manzili.Core.Services
+{
+    public class FileService
+    {
+
+        public async Task<string> UploadImageAsync(string targetFolder, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return "NoImage";
+
+            try
+            {
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory() + "/wwwroot", targetFolder);
+                Directory.CreateDirectory(folder);
+
+                var extension = Path.GetExtension(file.FileName);
+                var fileName = $"{Guid.NewGuid().ToString().Replace("-", string.Empty)}{extension}";
+
+                var filePath = Path.Combine(folder, fileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                    await fileStream.FlushAsync();
+                }
+
+                return $"/{targetFolder}/{fileName}";
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error: {ex.Message}");
+
+
+                return "FailedToUploadImage";
+            }
+        }
+    }
+}
+
+
