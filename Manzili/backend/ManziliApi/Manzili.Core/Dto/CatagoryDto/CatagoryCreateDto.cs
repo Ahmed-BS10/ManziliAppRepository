8f6e10ac896c67f07e7ae733fbcd64b62719d203
Ahmed-BS10 <157908930+Ahmed-BS10@users.noreply.github.com>
2025-02-04 23:29:30 +0300

    Add CatagoryCreateDto class for category creation
    Introduce CatagoryCreateDto class in Manzili.Core.Dto.CatagoryDto namespace.
    This class includes properties for category name and image file using IFormFile.
    Import necessary namespaces to support the class functionality.

---
+++ Manzili/backend/ManziliApi/Manzili.Core/Dto/CatagoryDto/CatagoryCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Core.Dto.CatagoryDto
+{
+    public class CatagoryCreateDto
+    {
+        public string Name { get; set; }
+
+        public IFormFile Image { get; set; }
+        
+    }
+}
