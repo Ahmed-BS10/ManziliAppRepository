14d6bedf2295d677586cf7d539b084ec2a1777d1
Ahmed-BS10 <157908930+Ahmed-BS10@users.noreply.github.com>
2025-02-16 01:18:37 +0300

    Add CreateRateDto record with UserId, StoreId, and Rate properties
    Introduce CreateRateDto.cs with necessary using directives.
    Create Manzili.Core.Dto.RateDto namespace.
    Define sealed record CreateRateDto with UserId, StoreId, and Rate.

---
+++ Manzili/backend/ManziliApi/Manzili.Core/Dto/RateDto/CreateRateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Core.Dto.RateDto
+{
+    public sealed record CreateRateDto(int UserId , int StoreId , int Rate);
+    
+}
