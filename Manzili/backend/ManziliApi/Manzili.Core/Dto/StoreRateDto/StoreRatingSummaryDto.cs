0d6448b7e9cef97e57fb39ab72ab007baeaae02d
Osama-Omer <108553757+Osama-Omer@users.noreply.github.com>
2025-03-18 17:43:33 +0300

    get rate and calculate rating

---
+++ Manzili/backend/ManziliApi/Manzili.Core/Dto/StoreRateDto/StoreRatingSummaryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Core.Dto.StoreRateDto
+{
+    public class StoreRatingSummaryDto
+    {
+        public int StoreId { get; set; }  
+        public double AverageRating { get; set; }  
+        public int TotalRatings { get; set; }  
+        public Dictionary<int, int> RatingsBreakdown { get; set; } = new();  
+    }
+}
---
+++ Manzili/backend/ManziliApi/Manzili.Core/Services/IStoreRateServices.cs
@@ -0,0 +1,15 @@
+﻿using Manzili.Core.Dto.StoreRateDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Core.Services
+{
+    interface IStoreRateServices
+    {
+        Task<OperationResult<CreateStoreRateDto>> CreateOrUpdate(CreateStoreRateDto createRateDto);
+        Task<OperationResult<StoreRatingSummaryDto>> GetStoreRatingsAsync(int storeId);
+    }
+}
